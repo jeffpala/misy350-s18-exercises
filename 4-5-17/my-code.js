2d17210aa3374cb71595ef8ff7c35e3fc809d130
Jeff <palajeffrey@gmail.com>
2018-04-05 10:37:57 -0400

    added js examples

---
+++ 4-5-17/my-code.js
@@ -0,0 +1,14 @@
+blist = ["go skydiving","visit office hours","get a pizza"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+
+console.log(blist.length);
+console.log(blist);
+
+/* for loop
+just a comment
+*/
+for (let i=0; i< blist.length; i++){
+  console.log(blist[i])
+}
