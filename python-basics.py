b523c53653e217f7f53da2d031af07880b462d70
Jeff <palajeffrey@gmail.com>
2018-04-19 10:24:00 -0400

    add exercise

---
+++ python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "hello world"
+name = "jeff"
+print greeting # this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %s! how are you doing, today" % (greeting,name)
